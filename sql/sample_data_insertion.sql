a464747e3c14ac924bdc556e7cbe521cc87afffe
John Swindell <102964572+John-Swindell@users.noreply.github.com>
2025-05-20 00:12:09 -0500

    Update sample_data_insertion.sql
    remove excess comments.

--- sql/sample_data_insertion.sql
+++ sql/sample_data_insertion.sql
@@ -1,6 +1,4 @@
 -- Sample data insertion with two imaginary students (John Doe and Jane Smith)
--- If you have forked this, keep the order of insertion the same for your own data
--- You'll mess up the foreign keys if you don't properly insert your data.
 
 -- My personal data is elsewhere.
 
@@ -30,9 +28,9 @@ INSERT INTO Degree (PersonID, EducationID, DegreeName, Major, OverallGPA, Honors
 INSERT INTO Degree (PersonID, EducationID, DegreeName, Major, OverallGPA, Honors) VALUES (2, 2, 'A.S. Web Development', 'Web Design', 3.50, NULL);
 
 -- 6. Insert PersonSkills // This is linking Persons to Skills through the junction table.
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 1); -- John Doe knows Python (SkillID 1)
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 3); -- John Doe knows SQL (SkillID 3)
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 4); -- John Doe knows PostgreSQL (SkillID 4)
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 1); -- Jane Smith knows Python (SkillID 1)
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 5); -- Jane Smith knows HTML (SkillID 5)
-INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 6); -- Jane Smith knows CSS (SkillID 6)
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 1);
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 3); 
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (1, 4); 
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 1); 
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 5);
+INSERT INTO PersonSkill (PersonID, SkillID) VALUES (2, 6);
